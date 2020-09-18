b477526258b70d886384efd5e410867e6fbf05b3
Cayo Diebe <cayodiebe@gmail.com>
2020-09-18 17:29:19 -0300

    .

---
+++ MinhaDemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaDemoMVC.Models;
+
+namespace MinhaDemoMVC.Controllers
+{
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Route("")]
+        [Route("pagina-inicial")]
+        public IActionResult Index(string id, string categoria)
+        {
+            return View();
+        }
+
+        [Route("privacidade")]
+        [Route("policida-de-privacidade")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("erro-encontrado")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
